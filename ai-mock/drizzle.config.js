97fdd3d4d43ae2872ec2438ba6225d96103cbe07
Amika Deshapriya <150609710+amika-d@users.noreply.github.com>
2025-03-24 11:54:33 +0530

    Delete ai-mock directory

--- ai-mock/drizzle.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-export default defineConfig({
-  schema: "./utils/schema.js",
-  dialect: "postgresql",
-  out: "./drizzle",
-  dbCredentials: {
-    url: 'postgresql://mock-int_owner:npg_QeDqES4La6fK@ep-delicate-poetry-a1jzw4rf-pooler.ap-southeast-1.aws.neon.tech/mock-int?sslmode=require',
-  }
-})
--- ai-mock/drizzle/0000_lowly_viper.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "mockInterview" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"jsonMockResp" text NOT NULL,
-	"jobRole" varchar NOT NULL,
-	"jobDesc" varchar NOT NULL,
-	"createdBy" varchar NOT NULL,
-	"createdAt" timestamp NOT NULL,
-	"mockId" varchar NOT NULL
-);
